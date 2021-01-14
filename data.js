ecdfdee716f3740814e0849abbc2ef198a65dea5
kathrynleee <50726368+kathrynleee@users.noreply.github.com>
2021-01-14 21:17:37 +0100

    Create data.js

---
+++ data.js
@@ -0,0 +1,57 @@
+{"nodes": [
+            {
+            "data": {"id": "a", "name": "Gene1"}
+            },
+            {
+            "data": {"id": "b", "name": "Gene2"}
+            },
+            {
+            "data": {"id": "c", "name": "Gene3"}
+            },
+            {
+            "data": {"id": "d", "name": "Gene4"}
+            },
+            {
+            "data": {"id": "e", "name": "Gene5"}
+            },
+            {
+            "data": {"id": "f", "name": "Gene6"}
+            }
+    ],
+            "edges": [
+            {
+            "data": {
+            "id": "ab",
+                    "source": "a",
+                    "target": "b"
+            }
+            },
+            {
+            "data": {
+            "id": "cd",
+                    "source": "c",
+                    "target": "d"
+            }
+            },
+            {
+            "data": {
+            "id": "ef",
+                    "source": "e",
+                    "target": "f"
+            }
+            },
+            {
+            "data": {
+            "id": "ac",
+                    "source": "a",
+                    "target": "d"
+            }
+            },
+            {
+            "data": {
+             "id": "be",
+                    "source": "b",
+                    "target": "e"
+                }
+                }]    
+    }
